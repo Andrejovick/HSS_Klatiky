891a74c147d72d6c874a5e1bc4abfbd17ab205cb
Andrejovick <belicandrej@gmail.com>
2024-06-11 19:45:49 +0200

    Add project files.

---
+++ HSS Klatiky/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace HSS_Klatiky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Column1,
+            this.Column2});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(263, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(588, 794);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1093, 954);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(21, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Zmazať";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 10;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ID.Width = 50;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Dátum odlievania";
+            this.Column1.MinimumWidth = 40;
+            this.Column1.Name = "Column1";
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column1.Width = 120;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Certifikát";
+            this.Column2.MinimumWidth = 40;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column2.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 954);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ HSS Klatiky/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HSS_Klatiky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+            this.WindowState = FormWindowState.Maximized;
+        }       
+
+        private void InitializeDataGridView()
+        {
+            // Handle the CellValueChanged event
+            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
+
+            // Handle the CurrentCellDirtyStateChanged event to commit edit
+            dataGridView1.CurrentCellDirtyStateChanged += dataGridView1_CurrentCellDirtyStateChanged;
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check if the row index is valid
+            if (e.RowIndex < 0 || e.RowIndex >= dataGridView1.Rows.Count)
+            {
+                return; // Exit the method if the row index is invalid
+            }
+
+            // Check if the column index is valid
+            if (e.ColumnIndex != 1) // We are only interested in changes in the first column
+            {
+                return; // Exit the method if the column index is invalid
+            }
+
+            try
+            {
+                // Get the value of the cell in the first column
+                var secondColumnValue = dataGridView1.Rows[e.RowIndex].Cells[1].Value?.ToString();
+
+                // Use a switch statement to handle different cases
+                switch (secondColumnValue)
+                {
+                    case "22/36":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022/132";
+                        break;
+
+                    case "2213":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022/132";
+                        break;
+
+                    case "2214":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022/132";
+                        break;
+
+                    case "2237":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022-142";
+                        break;
+
+                    case "2240":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022-142, 2022-146";
+                        break;
+                    
+                    case "2241":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022-146";
+                        break;
+
+                    case "2244":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022-150";
+                        break;
+
+                    case "2245":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022-150";
+                        break;
+
+                    case "2246":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022/153";
+                        break;
+
+                    case "2247":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2022/153";
+                        break;
+
+                    case "22/51":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/03";
+                        break;
+
+                    case "22/52":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/03";
+                        break;
+
+                    case "2305":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/16";
+                        break;
+
+                    case "2306":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/16";
+                        break;
+
+                    case "2307":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/17, 2023/26";
+                        break;
+
+                    case "2308":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/17, 2023/23, 2023/26";
+                        break;                    
+
+                    case "2311":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/23";
+                        break;
+
+                    case "2309":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/27";
+                        break;
+
+                    case "2310":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/27";
+                        break;
+
+                    case "2314":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023-33";
+                        break;
+
+                    case "2316":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/35";
+                        break;
+
+                    case "2317":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/35";
+                        break;
+
+                    case "3164":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023-38";
+                        break;
+
+                    case "2334":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023-77";
+                        break;
+
+                    case "2335":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023-78, 2023-79";
+                        break;
+
+                    case "2336":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023-80";
+                        break;
+
+                    case "2337":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023-81, 2023-82";
+                        break;
+
+                    case "2339":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023-83, 2023/87";
+                        break;
+
+                    case "2341":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/87";
+                        break;
+
+                    case "2345":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/92";
+                        break;
+
+                    case "2346":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/92";
+                        break;
+
+                    case "3483":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/95";
+                        break;
+
+                    case "3484":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/95";
+                        break;
+
+                    case "3485":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/96";
+                        break;
+
+                    case "3493":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2023/96";
+                        break;
+
+                    case "3494":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/02";
+                        break;
+
+                    case "3501":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/02";
+                        break;
+
+                    case "4023":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/03";
+                        break;
+
+                    case "4029":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/03";
+                        break;
+
+                    case "4025":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/07";
+                        break;
+
+                    case "4033":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/07";
+                        break;
+
+                    case "4040":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/08";
+                        break;
+
+                    case "4043":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/08, 2024/12";
+                        break;
+
+                    case "4045":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/12";
+                        break;
+
+                    case "4063":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/13, 2024/14";
+                        break;
+
+                    case "4052":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/13, 2024/19";
+                        break;
+
+                    case "4072":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/14";
+                        break;
+
+                    case "4074":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/15, 2024/19, 2024/32";
+                        break;
+
+                    case "4075":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/15, 2024/16, 2024/20, 2024/32";
+                        break;
+
+                    case "4084":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/20, 2024/21, 2024/33";
+                        break;
+
+                    case "4111":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/21, 2024/26";
+                        break;
+
+                    case "4112":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/22, 2024/27";
+                        break;
+
+                    case "4113":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/22, 2024/27";
+                        break;
+
+                    case "4091":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/33";
+                        break;
+
+                    case "4092":
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = "2024/34";
+                        break;
+
+                    default:
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = string.Empty; // Or any default value
+                        break;
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                // Handle the exception (optional logging or user notification)
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void dataGridView1_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            // This will commit the edit in the cell, which will trigger CellValueChanged event
+            if (dataGridView1.IsCurrentCellDirty)
+            {
+                dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+            }
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            // Update the ID column with row numbers
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                dataGridView1.Rows[i].Cells["ID"].Value = i + 1;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+    }
+}
